0dc362a63a6d2d55a4d736a2d14c38566ea699de
Akira Yamamoto <3007213+akirayamamoto@users.noreply.github.com>
2025-05-30 06:31:12 +1000

    DEMO-123: Aumentou o preco do peixe (#2)

--- OctopusSamples.OctoPetShop.ProductService.Tests/Controllers/ProductsControllerTests.cs
+++ OctopusSamples.OctoPetShop.ProductService.Tests/Controllers/ProductsControllerTests.cs
@@ -31,11 +31,11 @@ namespace OctopusSamples.OctoPetShop.ProductService.Tests
             },
             new ProductDetail
             {
-                Id = 4,
+                Id = 3,
                 Name = "Fish",
                 Description = "Goldie the goldfish",
                 ImageUrl = "https://s3.amazonaws.com/i.octopus.com/videos/Fish.png",
-                Price = 20.0,
+                Price = 25.0,
                 InStock = true
             },
             new ProductDetail
